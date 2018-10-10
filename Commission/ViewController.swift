e79a2d83d072ae9144b52809852f3d75e87a015d
Kyle Chung <kchung1068@jh-mba158-04.d214.org>
2018-10-10 14:35:47 -0500

    Added if let to get input

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
     @IBOutlet var totalPayLabel: UILabel!
     @IBOutlet var commissionPayTextField: UITextField!
+    let basePay:Double = 500
     
-    override func viewDidLoad() {
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+    
         
     }
 
 
-    @IBAction func calculateOnTapped(_ sender: UIButton) {
+    @IBAction func calculateOnTapped(_ sender: UIButton)
+    {
+        getInput()
+        
     }
+    func getInput(){
+        if let data = commissionPayTextField.text, let commissionPay = Double(data){
+            print(("everything is fine"))
+        } else {
+            print("error occured")
+            
+        }
+        
+        
+    }
+    
 }
 
